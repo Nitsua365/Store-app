8e36e9218b3e9032ed05756cfce07daa4eb423c4
Nitsua365 <austinblanchard@hotmail.com>
2022-03-08 18:19:15 -0600

    updating

--- scraping/storeInDB.js
+++ scraping/storeInDB.js
@@ -2,21 +2,18 @@ const dbClient = require('./databaseClient/dbClient').pool;
 
 module.exports = {
   storeAmazonDepartment : (name, subDepartments, URL) => {
-    const subDepartmentsStr = subDepartments.length === 0 ? '' : subDepartments.join(';');
+    const subDepartmentsStr = subDepartments.length === 0 ? 'NULL' : subDepartments.join(';');
 
-    const insertQuery = {
-      text: `INSERT INTO amazon_department(name,subdepartments,link) VALUES('$1','$2','$3');`,
-      values: [name, subDepartmentsStr, URL]
-    }
-
-    dbClient.query(insertQuery, (err, res) => {
+    const insertQuery = `INSERT INTO amazon_department(name, subdepartments, link) VALUES($1,$2,$3);`
+    
+    dbClient.query(insertQuery, [name, subDepartmentsStr, URL]).then((err, res) => {
       if (err) {
         console.error(err);
       }
       else {
         console.log(res);
       }
-    });
+    })
 
   }
 }
