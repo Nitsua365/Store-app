af5094c3cf70d0d08d7c929cf4eae8d95286671b
Austin Blanchard <42254605+Nitsua365@users.noreply.github.com>
2022-07-10 00:15:59 -0700

    added affiliate link scraping and query string cleaning

--- scraping/pythonScrape/AmazonScraper/AmazonScraper/items.py
+++ scraping/pythonScrape/AmazonScraper/AmazonScraper/items.py
@@ -1,5 +1,9 @@
 import scrapy
 from scrapy.loader import ItemLoader
+from urllib.parse import urlparse
+
+from requests.models import PreparedRequest
+
 from itemloaders.processors import TakeFirst, MapCompose
 from w3lib.html import remove_tags
 
@@ -21,6 +25,26 @@ def ratingClean(string):
 def appendAmazonURL(string):
     return 'https://www.amazon.com' + string
 
+def cleanQueryParams(URL):
+    url_path = urlparse(URL).path
+    url_prot = urlparse(URL).scheme
+    url_web = urlparse(URL).netloc
+
+    def cleanPath(path_URL):
+        split_url = urlparse(path_URL).path.split('/')
+        split_url.pop(len(split_url) - 1)
+        split_url.pop(0)
+        return '/'.join(split_url)
+
+    url_path = cleanPath(URL) if 'ref=' in URL else url_path
+
+    return url_prot + '://' + url_web + '/' + url_path
+
+def addAffiliateParams(URL):
+    req = PreparedRequest()
+    req.prepare_url(url=URL, params={ 'tag': 'amerizon02-20', 'language': 'en_US', 'linkCode': 'll1' })
+    return req.url
+
 class AmazonscraperItem(scrapy.Item):
     # define the fields for your item here like:
     asin = scrapy.Field(input_processor=MapCompose(remove_tags, initialClean), output_processor=TakeFirst())
@@ -28,7 +52,8 @@ class AmazonscraperItem(scrapy.Item):
     price = scrapy.Field(input_processor=MapCompose(remove_tags, initialClean, removeDollarSigns), output_processor=TakeFirst())
     department = scrapy.Field(input_processor=MapCompose(remove_tags, initialClean, removeHTMLChars, initialClean), output_processor=TakeFirst())
     rating = scrapy.Field(input_processor=MapCompose(remove_tags, initialClean, ratingClean), output_processor=TakeFirst())
-    productpagelink = scrapy.Field(input_processor=MapCompose(remove_tags, initialClean, removeHTMLChars, appendAmazonURL), output_processor=TakeFirst())
+    productpagelink = scrapy.Field(input_processor=MapCompose(remove_tags, initialClean, removeHTMLChars, appendAmazonURL, cleanQueryParams), output_processor=TakeFirst())
+    affiliatelink = scrapy.Field(input_processor=MapCompose(remove_tags, initialClean, removeHTMLChars, addAffiliateParams), output_processor=TakeFirst())
     picturereflink = scrapy.Field(input_processor=MapCompose(remove_tags, initialClean, removeHTMLChars), output_processor=TakeFirst())
     manufacturer = scrapy.Field(input_processor=MapCompose(remove_tags, initialClean, removeHTMLChars), output_processor=TakeFirst())
     countryoforigin = scrapy.Field(input_processor=MapCompose(remove_tags, initialClean, removeHTMLChars), output_processor=TakeFirst())
