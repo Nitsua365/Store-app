3a141e577cd8deefe08b242149a4672f7cdca194
Austin Blanchard <42254605+Nitsua365@users.noreply.github.com>
2022-07-23 23:41:03 -0500

    Changed ignore

--- americazon/utils/fetchSearch.js
+++
@@ -1,4 +0,0 @@
-export default async function fetchSearch( query ) {
-    const res = await fetch(`/api/products/searchProducts?${(new URLSearchParams(query)).toString()}`);
-    return res.json();
-}
--- scraping/pythonScrape/csvCleaning/redisToCSV.sh
+++
@@ -1,4 +0,0 @@
-redis-cli --scan --pattern amazon_products:* |\
-grep -e "^amazon_products:[^:]*$"
-awk '{print "hmget " $0 " rating productpagelink department manufacturer datescrapped countryoforigin picturereflink productname affiliatelink price"}' |\
-redis-cli --csv > amazon_products.csv
