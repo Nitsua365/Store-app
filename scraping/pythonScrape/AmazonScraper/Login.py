33bad04950da07808f9cbc46d712c80ade668f52
Austin Blanchard <42254605+Nitsua365@users.noreply.github.com>
2022-07-23 23:38:03 -0500

    changed login

--- scraping/pythonScrape/AmazonScraper/Login.py
+++ scraping/pythonScrape/AmazonScraper/Login.py
@@ -7,14 +7,15 @@ chromeData = {
     'data': '/Users/austinblanchard/Library/Application Support/Google/Chrome/'
 }
 
-postgres = {
-    'dbname': 'americazon',
-    'user': 'austinblanchard'
-}
-
 redis = {
     'host': 'redis-11267.c275.us-east-1-4.ec2.cloud.redislabs.com',
     'port': 11267,
     'db': 0,
     'password': 'uC01x09XShZbMe5qTjQneegt8sxqxqFP'
+}
+
+algolia = {
+    'API_KEY': 'b34829cc44639b18b05179e13050d5e5',
+    'APP_ID': 'NNQKCWQ55R',
+    'SEARCH_INDEX': "amazon_products"
 }
