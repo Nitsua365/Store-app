0350f9d3b6764c8a7051bed5da0397389fe5904f
Nitsua365 <austinblanchard@hotmail.com>
2022-09-14 12:12:08 -0500

    added more concurrent requests per proxy

--- scraping/pythonScrape/AmazonScraper/AmazonScraper/spiders/productspider.py
+++ scraping/pythonScrape/AmazonScraper/AmazonScraper/spiders/productspider.py
@@ -9,9 +9,7 @@ import logging
 class ProductspiderSpider(scrapy.Spider):
     name = 'productspider'
     allowed_domains = ['amazon.com']
-    start_urls = ['https://www.amazon.com/s?keywords=Outdoor+Lighting+Products&i=tools&rh=n%3A495236%2Cp_85%3A2470955011%2Cp_6%3AATVPDKIKX0DER&dc&c=ts&qid=1661663488&rnid=339807011&ts_id=495236&ref=sr_nr_p_6_2&ds=v1%3A8aXjtrdGRF%2FJE6nYyv3etBTBxPYPDk%2F%2BrELVEkPnSLY',
-                    'https://www.amazon.com/s?i=appliances&bbn=2619525011&rh=p_85%3A2470955011%2Cp_6%3AATVPDKIKX0DER&dc&ds=v1%3AVnKBUr0JeWu6dY7fVHQmY11u8yT9UTTjfU5uymETD9Q&crid=XVGWM75XPEF9&qid=1661664126&rnid=2661622011&sprefix=%2Cappliances%2C171&ref=sr_nr_p_6_2',
-                    'https://www.amazon.com/s?i=office-products&bbn=1064954&rh=p_85%3A2470955011%2Cp_6%3AATVPDKIKX0DER&dc&ds=v1%3Abex62%2B%2FZGDZNQowek5TbTbKLwgbeyVje8Qz1Gazz2SE&crid=X29UGURKS7SI&qid=1661664298&rnid=331539011&sprefix=%2Coffice-products%2C103&ref=sr_nr_p_6_1']
+    start_urls = ['https://www.amazon.com/s?i=hpc&bbn=3760901&rh=p_85%3A2470955011%2Cp_6%3AATVPDKIKX0DER&dc&ds=v1%3AXD0ZxGV7QEG8N9Y3cT2RXisZ3IhpoDo5NItAIZKJDAA&crid=160RQC2UUK33F&qid=1663174667&rnid=331556011&sprefix=%2Chpc%2C89&ref=sr_nr_p_6_1', 'https://www.amazon.com/s?i=industrial&bbn=16310091&rh=p_85%3A2470955011%2Cp_6%3AATVPDKIKX0DER%2Cp_72%3A1248921011&dc&crid=1PU3IJVKDPZKT&qid=1663174691&rnid=1248919011&sprefix=%2Cindustrial%2C87&ref=sr_nr_p_72_1&ds=v1%3ANSVJvDeh9aU9Zqk16%2FW5uu6lYDtE4A4lbNacMtenNX4', 'https://www.amazon.com/s?i=toys-and-games&bbn=165793011&rh=p_85%3A2470955011%2Cp_6%3AATVPDKIKX0DER%2Cp_72%3A1248963011&dc&crid=3S8RCWSO0YP9F&qid=1663174753&rnid=1248961011&sprefix=%2Ctoys-and-games%2C88&ref=sr_nr_p_72_1&ds=v1%3AObUBq93ZMSWNrJMlydUpb0xTc9UbV1niLx7PVJGNwK0', 'https://www.amazon.com/s?i=mobile&bbn=2335752011&rh=p_85%3A2470955011%2Cp_6%3AATVPDKIKX0DER%2Cp_72%3A2491149011&dc&crid=23ZGNT6PQYHLR&qid=1663174827&rnid=2491147011&sprefix=%2Cmobile%2C83&ref=sr_nr_p_72_1&ds=v1%3AIo8GePc7u4fvHPTc%2F46FAt0XZCRxT0r24yWM9ZBIkH4']
 
     def getPageFields(self, response, item):
         # scrape country of origin
