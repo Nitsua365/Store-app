08127aafc1ee4392741bf475b84cb14847670a8a
Austin Blanchard <42254605+Nitsua365@users.noreply.github.com>
2023-01-07 17:34:26 -0600

    setting up browser extension

--- americazon/pages/api/index.js
+++ americazon/pages/api/index.js
@@ -1,6 +1,16 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
+import NextCors from 'nextjs-cors';
+
 export default async function handler(req, res) {
+
+  await NextCors(req, res, {
+    // Options
+    methods: ['GET'],
+    origin: ['http://192.168.0.199'],
+    optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
+ });
+
   // respond with valid status
   res.status(200).json({ message: "Welcome to the Americazon API" });
 }
