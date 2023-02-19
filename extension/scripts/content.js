4631e73c3f363623a254c778c5cf28523c84e55f
Austin Blanchard <42254605+Nitsua365@users.noreply.github.com>
2023-02-19 16:44:14 -0600

    cleaning

--- extension/scripts/content.js
+++ extension/scripts/content.js
@@ -44,31 +44,6 @@ async function getCOO() {
   // persist to local cache
   if (result) Object.entries(result).forEach(([asin, COO]) => sessionStorage.setItem(asin, COO))
 
-  // add COO to the UI
-  // for (let i = 0; i < asins.length; i++) {
-  //   console.log(sessionStorage.getItem(asins[i]) || result[asins[i]])
-  //   let productElem = document.querySelector(`[data-asin='${asins[i]}']`)
-  //   if (productElem) {
-  //     let div = document.createElement('div');
-  //     div.textContent = `Country of Origin: ${sessionStorage.getItem(asins[i]) || result[asins[i]]}`
-
-  //     div.style.color = "black"
-  //     div.style.padding = "2px"
-  //     div.style.borderRadius = "5px"
-  //     div.style.background = "#febd69"
-  //     div.style.borderWidth = "5px"
-  //     div.style.borderColor = "grey"
-  //     div.style.maxWidth = "90%"
-  //     div.style.marginLeft = "5px"
-
-  //     productElem.style.marginBottom = "50px"
-
-  //     productElem.appendChild(div)
-  //   }
-  // }
-
-  // await chrome.runtime.sendMessage({ "status": "done" })
-
 }
 
 setTimeout(function() {
