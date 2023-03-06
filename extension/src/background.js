0912cc34b17e51f04ff3d36b094adbd4dbca0d44
Austin Blanchard <42254605+Nitsua365@users.noreply.github.com>
2023-03-05 20:48:22 -0600

    cleaning and optimizing background js

--- extension/src/background.js
+++ extension/src/background.js
@@ -3,6 +3,8 @@ import { DOMParser } from '@xmldom/xmldom';
 
 const fetchASIN = async (asins) => {
 
+  if (!asins.length) return;
+
   const filterCOO = (str) => str.replace('\n', '').replace('&lrm;', '').trim()
   const parseCOO = async (html) => filterCOO(
     xpath.select1("//*[contains(text(), 'Country of Origin') or contains(text(), 'Country/Region of origin')]//following-sibling::*",
@@ -16,26 +18,18 @@ const fetchASIN = async (asins) => {
     }).parseFromString(html, 'text/html'),
     )?.firstChild?.data || '',
   )
-
-  if (!asins.length) return;
   
   // get the asin URLs
   const asinURLS = asins.map(asin => `https://www.amazon.com/dp/${asin}`)
   
   // fetch the URLS
-  const fTime = performance.now()
   const req = await Promise.all(asinURLS.map((url) => fetch(url)))
-  console.log(`fetch time: ${(performance.now() - fTime)}`);
 
   // resolve them to text
-  const t1 = performance.now()
   const productsHTML = await Promise.all(req.map((res) => res.text()))
-  console.log(`text time: ${performance.now() - t1}`);
     
-  const t2 = performance.now()
   // parse productsHTML pages to get necessary data ie: COO
   const productCOO = await Promise.all(productsHTML.map(parseCOO))
-  console.log(`parsing HTML: ${performance.now() - t2}`);
 
   // create the result 
   const result = {}
